9d2cce5e7de43b5df712e7df6133aebd714f5c86
Gabryel Alves <alvesgabryel086@gmail.com>
2024-11-19 00:36:55 -0300

    Savando aula 7

---
+++ AulasExerc/aula7.js
@@ -0,0 +1,24 @@
+// nomeie 3 propriedades ou métodos de strings
+var nome = "Robison";
+
+//console.log(nome.charAt(3));
+//console.log(nome.length);
+//console.log(nome.replace('Ro', 'Jo'));
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+
+var btn = document.querySelector('.btn');
+
+// console.log(btn)
+
+//ariaDisabled
+//innerHTML
+//attributes
+//addEventListener
+// id;
+
+
+// busque na web um objeto (método) capaz de interagir com o clipboard, 
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
+
+// https://clipboardjs.com
